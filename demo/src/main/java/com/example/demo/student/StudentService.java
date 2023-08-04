1f8f501582361f4497465f161e59ee011134ab05
yoman321 <luophilip@outlook.com>
2023-08-04 13:15:12 -0400

    added controller and service class

---
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,22 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 01),
+                        21
+                )
+        );
+    }
+}

222d5c849f04c8acf5a197d83ae98c726402dbec
yoman321 <luophilip@outlook.com>
2023-08-05 18:53:18 -0400

    issue: need to properly connect to psql server before continuing tutorial

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
+//Note: need to connect to psql with command line before continuing tutorial
 @SpringBootApplication
 public class DemoApplication {
 
